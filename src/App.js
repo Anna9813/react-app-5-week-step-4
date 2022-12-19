9d6d4e385a558ee61b03f1a3ffaf2e391d2f4077
Anna9813 <108585944+Anna9813@users.noreply.github.com>
2022-12-19 21:12:17 +0200

    Added backspace

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return <div className="App">
     <h1>Weather App</h1> 
     <footer>
-      This project was coded by Hanna Soloviova and is
+      This project was coded by Hanna Soloviova and is{" "}
       <a href="https://github.com/Anna9813/react-app-5-week-step-4" target="_blank">
         open-sourced on GitHub
       </a>

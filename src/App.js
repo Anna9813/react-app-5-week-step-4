19b829b7f97ae49824d0b184b8e5031c6353c231
Anna9813 <108585944+Anna9813@users.noreply.github.com>
2022-12-19 20:59:19 +0200

    Added the GitHub

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return <div className="App">
+    <h1>Weather App</h1> 
+    <footer>
+      This project was coded by Hanna Soloviova and is
+      <a href="https://github.com/Anna9813/react-app-5-week-step-4" target="_blank">
+        open-sourced on GitHub
+      </a>
+  
+  </footer>
+  </div>;
 }
 

71255df2593cd9cc45e4ac62c7fcd8eb09ccdc9c
Anna9813 <108585944+Anna9813@users.noreply.github.com>
2022-12-20 22:19:47 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return <div className="App">
     <div className="container">
     <h1>Weather App</h1> 
+    <Weather />
     <footer>
       This project was coded by Hanna Soloviova and is{" "}
       <a href="https://github.com/Anna9813/react-app-5-week-step-4" target="_blank">

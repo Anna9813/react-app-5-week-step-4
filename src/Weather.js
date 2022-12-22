538de50b1c96ebdd52a27c6b527ca8276f20491b
Anna9813 <108585944+Anna9813@users.noreply.github.com>
2022-12-22 21:47:12 +0200

    add

--- src/Weather.js
+++ src/Weather.js
@@ -1,67 +1,69 @@
-import React, {useState} from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
-    const [weatherData, setWeatherData] = useState({ready: false});
-    const [city, setCity] = useState(props.defaultCity);
-    function handleResponse(response) {
-        setWeatherData({
-            ready: true,
-            temperature: response.data.main.temp,
-            humidity: response.data.main.humidity,
-            description: response.data.weather[0].description,
-            date: new Date(response.data.dt * 1000),
-            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-            wind: response.data.wind.speed,
-            city: response.data.name
-        });
-    }
-    function search() {
-        const apiKey = "4aad7cfbf82d50ad60b403e30cb79427";
-        let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-        axios.get(apiUrl).then(handleResponse);  
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-    }
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        search(city);
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
-    function handleCityChange(event) {
-        setCity(event.target.value);
-    }
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
 
-    if (weatherData.ready) {
-        return (
-        <div className="Weather">
-            <form onSubmit={handleSubmit}>
-                <div className="row">
-                    <div className="col-9">
-                    <input 
-                    type="search" 
-                    placeholder="Enter a city..." 
-                    className="form-control"
-                    autoFocus="on"
-                    onCharge="handleCityChange"
-                    />
-                </div>
-                <div className="col-3">
-                    <input 
-                    type="submit" 
-                    value="Search" 
-                    className="btn btn-primary w-100" />
-                </div>
+  function search() {
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
             </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
         </form>
-
         <WeatherInfo data={weatherData} />
-    </div>
+      </div>
     );
-    } else {
-        search();
-        return "Loading...."
-    }
-} 
+  } else {
+    search();
+    return "Loading...";
+  }
+}
